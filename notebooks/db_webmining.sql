cd23dab0010ae1edc6bd450465b7e0bc2cf793c8
Joe-515 <57894766+Joe-515@users.noreply.github.com>
2025-03-13 14:45:11 +0100

    organisation

--- notebooks/db_webmining.sql
+++ notebooks/db_webmining.sql
@@ -1,33 +1,18 @@
-CREATE TABLE postes (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS annonces(
+    reference TEXT PRIMARY KEY,
     intitule_poste TEXT NOT NULL,
     description TEXT,
     experience TEXT,
-    langue TEXT,
-    permis TEXT,
-    divers TEXT,
-    reference TEXT UNIQUE
+    divers TEXT    
 );
-CREATE TABLE competences (
+CREATE TABLE IF NOT EXISTS competences (
     id SERIAL PRIMARY KEY,
     nom TEXT NOT NULL UNIQUE,
     requis BOOLEAN NOT NULL
 );
 
-CREATE TABLE savoirs (
-    id SERIAL PRIMARY KEY,
-    nom TEXT NOT NULL UNIQUE,
-    requis BOOLEAN NOT NULL
-);
-
-CREATE TABLE poste_competences (
-    poste_id INT REFERENCES postes(id) ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS annonce_competences (
+    annonce_reference TEXT REFERENCES annonces(reference) ON DELETE CASCADE,
     competence_id INT REFERENCES competences(id) ON DELETE CASCADE,
-    PRIMARY KEY (poste_id, competence_id)
-);
-
-CREATE TABLE poste_savoirs (
-    poste_id INT REFERENCES postes(id) ON DELETE CASCADE,
-    savoirs_id INT REFERENCES savoirs(id) ON DELETE CASCADE,
-    PRIMARY KEY (poste_id, savoirs_id)
+    PRIMARY KEY (annonce_reference, competence_id)
 );
