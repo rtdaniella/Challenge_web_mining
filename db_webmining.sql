e6f313ed4ba9fa283886e08f5ed2d67584a0f4ea
Joe-515 <57894766+Joe-515@users.noreply.github.com>
2025-03-13 14:39:48 +0100

    upload annonces
    schema des tables
    contenu des tables au format csv

---
+++ db_webmining.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS annonces(
+    reference TEXT PRIMARY KEY,
+    intitule_poste TEXT NOT NULL,
+    description TEXT,
+    experience TEXT,
+    divers TEXT    
+);
+CREATE TABLE IF NOT EXISTS competences (
+    id SERIAL PRIMARY KEY,
+    nom TEXT NOT NULL UNIQUE,
+    requis BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS annonce_competences (
+    annonce_reference TEXT REFERENCES annonces(reference) ON DELETE CASCADE,
+    competence_id INT REFERENCES competences(id) ON DELETE CASCADE,
+    PRIMARY KEY (annonce_reference, competence_id)
+);
